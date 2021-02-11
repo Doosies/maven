19b4f5a5559bf517e915db30a5526dba95e7671b
Doosies <song961003@gmail.com>
2021-02-11 21:45:04 +0900

    Button Demo

--- front/src/MainPage.js
+++ front/src/MainPage.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled, {ThemeProvider} from 'styled-components';
-import CustomButton from './components/Button';
+import Button from './components/Button';
+import Dialog from './components/Dialog';
 
 // const Circle = ()=><div>hi</div>;
 const AppBlock = styled.div`
@@ -11,8 +12,27 @@ const AppBlock = styled.div`
     padding: 1rem;
 `;
 
+const ButtonGroup = styled.div `
+  & + & {
+    margin-top: 1rem;
+  }
+`;
+
 
 function MainPage() {
+  const [dialog, setDialog] = useState(false);
+  const onClick = ()=>{
+    setDialog(true);
+  }
+  const onConfirm = ()=>{
+    console.log("확인");
+    setDialog(false);
+  }
+  const onCancel = ()=>{
+    console.log("취소");
+    setDialog(false);
+  }
+
     return (
         <ThemeProvider
           theme={{
@@ -24,10 +44,52 @@ function MainPage() {
           }}
         >
           <AppBlock>
-            <CustomButton>BUTTON</CustomButton>
-            <CustomButton color="gray">BUTTON</CustomButton>
-            <CustomButton color="pink">BUTTON</CustomButton>
+            <ButtonGroup>
+              <Button size="large">BUTTON</Button>
+              <Button>BUTTON</Button>
+              <Button size="small">BUTTON</Button>
+            </ButtonGroup>
+
+            <ButtonGroup>
+              <Button color="gray" size="large">BUTTON</Button>
+              <Button color="gray">BUTTON</Button>
+              <Button color="gray" size="small">BUTTON</Button>
+            </ButtonGroup>
+
+            <ButtonGroup>
+              <Button color="pink" size="large">BUTTON</Button>
+              <Button color="pink">BUTTON</Button>
+              <Button color="pink" size="small">BUTTON</Button>
+            </ButtonGroup>
+
+            <ButtonGroup>
+              <Button c size="large" outline>BUTTON</Button>
+              <Button color="gray" outline>BUTTON</Button>
+              <Button color="pink" size="small" outline>BUTTON</Button>
+            </ButtonGroup>
+
+            <ButtonGroup>
+              <Button colos="blue" size="large" fullWidth>
+                BUTTON
+              </Button>
+              <Button color="pink" size="large" fullWidth>
+                BUTTON
+              </Button>
+              <Button color="gray" size="large" onClick={onClick} fullWidth>
+                삭제하기
+              </Button>
+            </ButtonGroup>
           </AppBlock>
+
+          <Dialog title="정말로 삭제 하시겠습니까?"
+                  onConfirm={onConfirm}
+                  onCancel={onCancel}
+                  visible={dialog}
+
+          >
+            데이터를 정말로 삭제 하시겠습니까?
+          </Dialog>
+  
         </ThemeProvider>
     );
 
--- front/src/components/Button.js
+++ front/src/components/Button.js
@@ -2,6 +2,74 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import { darken, lighten } from 'polished';
 
+const sizes = {
+    large:{
+        height: '3rem',
+        fontSize: '1.25rem'
+    },
+    medium:{
+        height: '2.25rem',
+        fontSize: '1rem'
+    },
+    small:{
+        heigh: '1.75rem',
+        fontSize: '0.875rem'
+    }
+}
+const sizeStyles = css`
+    ${(props)=>
+        css `
+            height: ${sizes[props.size].height};
+            font-size: ${sizes[props.size].fontSize};
+        `
+    }
+`;
+
+const colorStyles = css`
+    /* 색상 */
+    ${( {theme, color} )=>{
+        const selected = theme.color[color];
+        return css`
+            background: ${selected};
+            &:hover{
+                background: ${lighten(0.1, selected)};
+            }
+            &:active{
+                background: ${darken(0.1, selected)}
+            }
+            ${props=>
+                props.outline &&
+                css `
+                    color: ${selected};
+                    background: none;
+                    border: 1px solid ${selected};
+                    &:hover{
+                        background: ${selected};
+                        color: white;
+                    }
+                    &:active{
+                        background: ${darken(0.1, selected)}
+                    }
+                `}
+        `;
+    }}
+`;
+
+const fullWidthStyle = css `
+    ${(props)=>
+        props.fullWidth &&
+        css `
+            width: 100%;
+            justify-content: center;
+            ${'' /* & &{ */}
+            &:not(:first-child){
+                margin-left: 0;
+                margin-top: 1rem;
+            }
+        `
+    }
+`;
+
 const StyledButton = styled.button`
     /* 공통스타일 */
     display : inline-flex;
@@ -13,42 +81,34 @@ const StyledButton = styled.button`
     cursor: pointer;
     padding-left : 1rem;
     padding-right: 1rem;
-
-    /* 크기 */
-    height: 2.25rem;
-    font-size: 1rem;
-
-    /* 색상 */
-    ${props=>{
-        const selected = props.theme.color[props.color];
-        return css`
-            background: ${selected};
-            &:hover{
-                background: ${lighten(0.1, selected)};
-            }
-            &:active{
-                background: ${darken(0.1, selected)}
-            }
-        `;
-    }}
+    align-items: center;
+    /* margin-right: 1rem; */
 
     /* 기타 */
-    & + &{
-    /* &:not(:first-child){ */
+    /* &+&{ */
+    &:not(:first-child){
         margin-left : 1rem;
     }
+    /* 색상 */
+    ${colorStyles}
+    /* 크기 */
+    ${sizeStyles}
+
+    ${fullWidthStyle};
 `;
 
-function Button({children, ...rest}){
+function Button({children, color, size, outline, fullWidth, ...rest}){
     return(
-        <StyledButton {...rest}>
+        <StyledButton color={color} size={size} outline={outline} fullWidth={fullWidth} {...rest}>
             {children}
         </StyledButton>
     );
 }
 
 Button.defaultProps = {
-    color : 'blue'
+    color : 'blue',
+    size: 'medium',
+    // size : 'large'
 }
 
 export default Button;
