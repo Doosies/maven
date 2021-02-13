30de03015db802609b2f54cd6d90e203d6313042
Doosies <song961003@gmail.com>
2021-02-13 16:21:27 +0900

    state여러개

--- front/src/pages/studyPage.jsx
+++ front/src/pages/studyPage.jsx
@@ -1,18 +1,37 @@
 import React, { useState } from 'react';
 
 function StudyPage(){
-    const [text, setText] = useState('');
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: '',
+    });
+    const { name, nickname } = inputs;
 
-    const onChange = (event) =>{
-        setText(event.target.value);
+    const onChange = (e) =>{
+        const {value, name} = e.target;
+
+        setInputs({
+            ...inputs,
+            [name]:value
+        });
+
+        console.log(e.target.name);
+    }
+    const onReset = (e) =>{
+        setInputs({
+            name: '',
+            nickname: '',
+        });
     }
 
     return(
         <div>
-            <input/>
-            <button>초기화</button>
+            <input name="name" placeholder="이름" onChange={onChange} />
+            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
+            <button onClick={onReset}>초기화</button>
             <div>
-                <b>값:</b>
+                <b>값: </b>
+                {name} ({nickname})
             </div>
         </div>
     );
