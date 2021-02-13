c0655c1795f0047cd2d360de75d0f7d2d7b587c8
Doosies <song961003@gmail.com>
2021-02-13 16:25:50 +0900

    ref input(포커스)

--- front/src/pages/studyPage.jsx
+++ front/src/pages/studyPage.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 function StudyPage(){
     const [inputs, setInputs] = useState({
         name: '',
         nickname: '',
     });
+    const nameInput = useRef();
     const { name, nickname } = inputs;
 
     const onChange = (e) =>{
@@ -14,20 +15,22 @@ function StudyPage(){
             ...inputs,
             [name]:value
         });
-
-        console.log(e.target.name);
     }
     const onReset = (e) =>{
         setInputs({
             name: '',
             nickname: '',
         });
+
+        nameInput.current.focus();
     }
 
     return(
         <div>
-            <input name="name" placeholder="이름" onChange={onChange} />
-            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
+            <input name="name" placeholder="이름" 
+            onChange={onChange} value={name} ref={nameInput}/>
+            <input name="nickname" placeholder="닉네임" 
+            onChange={onChange} value={nickname}/>
             <button onClick={onReset}>초기화</button>
             <div>
                 <b>값: </b>
