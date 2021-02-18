fdc4813db026c8b1a846e827a9fb293cdd999caa
Doosies <song961003@gmail.com>
2021-02-18 19:48:43 +0900

    useAsync

--- front/src/components/APi/User.jsx
+++ front/src/components/APi/User.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import axios from 'axios';
-import useAsync from '../../hooks/useAsync';
+import { useAsync } from 'react-async';
+// import useAsync from '../../hooks/useAsync';
 
-async function getUser(id){
+async function getUser( {id }){
     const response = await axios.get(
         `https://jsonplaceholder.typicode.com/users/${id}`
     );
@@ -10,10 +11,15 @@ async function getUser(id){
 }
 
 function User({ id }){
-    const [state] =  useAsync(()=> getUser(id),[id]);
-    const {loading, data: user, error} = state;
+    const {data: user, error, isLoading } = useAsync({
+        promiseFn: getUser,
+        id,
+        watch: id,
+    })
+    // const [state] =  useAsync(()=> getUser(id),[id]);
+    // const {loading, data: user, error} = state;
 
-    if (loading) return <div>로딩중..</div>;
+    if (isLoading) return <div>로딩중..</div>;
     if (error) return <div>에러가 발생했습니다</div>;
     if (!user) return null;  
 
